8dfa779675740fd1aa624b5f13c574edea4d890f
John Min <john.c.min@gmail.com>
2014-01-23 00:44:05 -0500

    commit before upgrade to 13.10

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -11,6 +11,7 @@ class QuestionAdmin(admin.ModelAdmin):
 	('Date information', {'fields': ['pub_date']}),
     ]
     inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
 
 # Register your models here.
 admin.site.register(Question, QuestionAdmin)
